b342901c8f28fb24053ebb006042258d22f73c57
Jefferson Machado <jefferson@zeroai.com.br>
2026-08-10 01:04:30 -0300

    Adding schedule from south edition

--- src/regions/sul/index.tsx
+++ src/regions/sul/index.tsx
@@ -1,4 +1,5 @@
 import RegionPage from "@/regions/RegionPage";
+import type { Speaker } from "@/regions/types";
 import config from "./data/config.json";
 import organizers from "./data/organizers.json";
 import speakers from "./data/speakers.json";
@@ -9,10 +10,30 @@ import heroImage from "./assets/postcard-curitiba.png";
 import daianePhoto from "./assets/daiane.jpg";
 import leandroPhoto from "./assets/leandro.jpg";
 
+import muriloPhoto from "./assets/speakers/murilo.png";
+import lucianoPhoto from "./assets/speakers/luciano.jpg";
+import leoPhoto from "./assets/speakers/leo.png";
+import egonPhoto from "./assets/speakers/egon.jpg";
+import eduardoPhoto from "./assets/speakers/eduardo.jpg";
+import danPhoto from "./assets/speakers/dan.png";
+import agataPhoto from "./assets/speakers/agata.png";
+import robertoPhoto from "./assets/speakers/roberto.jpeg";
+
 const photoMap: Record<string, string> = {
   "daiane.jpg": daianePhoto,
   "leandro.jpg": leandroPhoto,
-  
+
+};
+
+const speakerPhotoMap: Record<string, string> = {
+  "murilo.png": muriloPhoto,
+  "luciano.jpg": lucianoPhoto,
+  "leo.png": leoPhoto,
+  "egon.jpg": egonPhoto,
+  "eduardo.jpg": eduardoPhoto,
+  "dan.png": danPhoto,
+  "agata.png": agataPhoto,
+  "roberto.jpeg": robertoPhoto,
 };
 
 const resolvedOrganizers = organizers.map((org) => ({
@@ -20,11 +41,16 @@ const resolvedOrganizers = organizers.map((org) => ({
   photo: photoMap[org.photo] || org.photo,
 }));
 
+const resolvedSpeakers: Speaker[] = speakers.map((speaker) => ({
+  ...speaker,
+  photo: speakerPhotoMap[speaker.photo] || speaker.photo,
+}));
+
 const Sul = () => (
   <RegionPage
     config={config}
     organizers={resolvedOrganizers}
-    speakers={speakers}
+    speakers={resolvedSpeakers}
     schedule={schedule}
     sponsors={sponsors}
     heroImage={heroImage}
